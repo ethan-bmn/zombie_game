ced4eb8f0073d6705a0999fa870c1054afba7bbf
ethan_bmn <ethan.bmn@hotmail.com>
2022-11-07 22:24:16 +0100

    Added player rotation to look at cursor

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use bevy::prelude::*;
-use bevy::input::mouse::*;
 use crate::player::*;
 use crate::setup::*;
 mod player;
 mod setup;
+
+#[derive(Default, Copy, Clone)]
+struct Angle(f32);
 
 fn main() {
     App::new()
@@ -17,16 +19,46 @@ fn main() {
         })
         .add_startup_system(setup)
         .add_system(update)
+        .insert_resource(Angle(0.0))
         .run();
 }
 
-fn update(mut _commands: Commands,
+fn update(keys: Res<Input<KeyCode>>,
           mut player_query: Query<&mut Transform, With<Player>>,
-          windows: Res<Windows>) {
-    for _transform in player_query.iter_mut() {
+          windows: Res<Windows>, mut angle: ResMut<Angle>) {
+    for mut transform in player_query.iter_mut() {
         let window = windows.get_primary().unwrap();
         if let Some(_position) = window.cursor_position() {
+            if keys.pressed(KeyCode::Right) {
+                angle.0 -= 1.0;
+            }
+            if keys.pressed(KeyCode::Left) {
+                angle.0 += 1.0;
+            }
+            println!("{}", get_angle(Vec2{
+                x: transform.translation.x,
+                y: transform.translation.y
+            }, window.cursor_position().unwrap()));
+
+            transform.rotation = Quat::from_rotation_z(get_angle(Vec2{
+                                                                                x: transform.translation.x,
+                                                                                y: transform.translation.y
+                                                                            },
+                                                                        Vec2{
+                                                                            x: window.cursor_position().unwrap().x-window.width()/2.0,
+                                                                            y: window.cursor_position().unwrap().y-window.height()/2.0
+                                                                        }));
+
 
         }
     }
+}
+
+fn get_angle(a: Vec2, b: Vec2) -> f32 {
+    let op = b.y - a.y;
+    let adj = b.x - a.x;
+    if op == 0.0 ||adj == 0.0 {
+        return 0.0;
+    }
+    return (op/adj).atan();
 }
