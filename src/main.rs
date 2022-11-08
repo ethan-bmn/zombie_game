53e470686cdb65235fb3a88e0b0814af7731d5b2
ethan_bmn <ethan.bmn@hotmail.com>
2022-11-08 23:26:37 +0100

    Added player entity resource implementation

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,6 @@ use crate::setup::*;
 mod player;
 mod setup;
 
-#[derive(Default, Copy, Clone)]
-struct Angle(f32);
-
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
@@ -17,48 +14,9 @@ fn main() {
             title: "zombie_game.rs".to_string(),
             ..default()
         })
-        .add_startup_system(setup)
-        .add_system(update)
-        .insert_resource(Angle(0.0))
+        .add_startup_system_to_stage(StartupStage::Startup, setup)
+        .add_startup_system(Player::setup)
+        .add_system(Player::update)
+        .insert_resource(PlayerRes)
         .run();
-}
-
-fn update(keys: Res<Input<KeyCode>>,
-          mut player_query: Query<&mut Transform, With<Player>>,
-          windows: Res<Windows>, mut angle: ResMut<Angle>) {
-    for mut transform in player_query.iter_mut() {
-        let window = windows.get_primary().unwrap();
-        if let Some(_position) = window.cursor_position() {
-            if keys.pressed(KeyCode::Right) {
-                angle.0 -= 1.0;
-            }
-            if keys.pressed(KeyCode::Left) {
-                angle.0 += 1.0;
-            }
-            println!("{}", get_angle(Vec2{
-                x: transform.translation.x,
-                y: transform.translation.y
-            }, window.cursor_position().unwrap()));
-
-            transform.rotation = Quat::from_rotation_z(get_angle(Vec2{
-                                                                                x: transform.translation.x,
-                                                                                y: transform.translation.y
-                                                                            },
-                                                                        Vec2{
-                                                                            x: window.cursor_position().unwrap().x-window.width()/2.0,
-                                                                            y: window.cursor_position().unwrap().y-window.height()/2.0
-                                                                        }));
-
-
-        }
-    }
-}
-
-fn get_angle(a: Vec2, b: Vec2) -> f32 {
-    let op = b.y - a.y;
-    let adj = b.x - a.x;
-    if op == 0.0 ||adj == 0.0 {
-        return 0.0;
-    }
-    return (op/adj).atan();
 }
--- src/player.rs
+++ src/player.rs
@@ -9,3 +9,51 @@ pub struct PlayerBundle {
 
 #[derive(Component)]
 pub struct Player;
+
+pub struct PlayerRes(pub Entity);
+
+pub trait Manage {
+	fn setup(commands: Commands,
+			 asset_server: Res<AssetServer>);
+
+	fn update(player_query: Query<&mut Transform, With<Player>>,
+			  windows: Res<Windows>);
+}
+
+impl Manage for Player {
+	fn setup(mut commands: Commands,
+			 asset_server: Res<AssetServer>) {
+		let player = commands.spawn_bundle(PlayerBundle {
+			player: Player,
+			sprite: SpriteBundle {
+				transform: Transform::from_xyz(0.0, 0.0, 0.0),
+				texture: asset_server.load("red.png"),
+				..default()
+			}
+		}).id();
+		commands.insert_resource(PlayerRes(player));
+	}
+
+	fn update(mut player_query: Query<&mut Transform, With<Player>>,
+			  windows: Res<Windows>) {
+		for mut transform in player_query.iter_mut() {
+			let window = windows.get_primary().unwrap();
+			if let Some(_position) = window.cursor_position() {
+				transform.rotation = Quat::from_rotation_z(get_angle(Vec2{
+					x: transform.translation.x,
+					y: transform.translation.y
+				},
+				 Vec2{
+					 x: window.cursor_position().unwrap().x-window.width()/2.0,
+					 y: window.cursor_position().unwrap().y-window.height()/2.0
+				 }));
+			}
+		}
+	}
+}
+
+fn get_angle(a: Vec2, b: Vec2) -> f32 {
+	let op = b.y - a.y;
+	let adj = b.x - a.x;
+	return (op/adj).atan();
+}
